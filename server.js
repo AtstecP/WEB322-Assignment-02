080ee6c4fa5035d6cf352843d241d5001dcba081
atstec <atstec80@gmail.com>
2025-02-17 21:45:08 -0500

    fix mistake with initializetion promise

--- server.js
+++ server.js
@@ -8,51 +8,59 @@
 *
 * Name: Viktor Makarov Student ID: 121659221 Date: 1/31/2025*
 ********************************************************************************/
-require("dotenv").config()
+require("dotenv").config();
 const projectData = require("./modules/projects");
 const express = require("express");
-const app = express(); 
-const path = require('path');
-const HTTP_PORT = process.env.PORT || 8080; 
+const app = express();
+const path = require("path");
+const HTTP_PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'));
-projectData.initialize();
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/home.html'));
-});
+projectData.initialize()
+  .then(() => {
+    
+    app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "/views/home.html"));
+    });
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/about.html'));
-});
+    app.get("/about", (req, res) => {
+      res.sendFile(path.join(__dirname, "/views/about.html"));
+    });
 
-app.get("/solutions/projects", (req, res) => {
-    const {sector} = req.query;
-    if (sector){
+    app.get("/solutions/projects", (req, res) => {
+      const { sector } = req.query;
+      if (sector) {
+        projectData
+          .getProjectsBySector(sector)
+          .then(data => res.send(data))
+          .catch(err => res.status(500).send(err));
+      } else {
+        projectData
+          .getAllProjects()
+          .then(data => res.send(data))
+          .catch(err => res.status(500).send(err));
+      }
+    });
+
+    app.get("/solutions/projects/:projectId", (req, res) => {
+      const { projectId } = req.params;
       projectData
-      .getProjectsBySector(sector)
-      .then(data => res.send(data))
-      .catch(err => res.status(500).send(err))
-    }else{
-      projectData
-      .getAllProjects()
-      .then(data => res.send(data))
-      .catch(err => res.status(500).send(err))
-    } 
-   
-})
+        .getProjectById(projectId)
+        .then(data => res.send(data))
+        .catch(err => res.status(500).send(err));
+    });
 
-app.get("/solutions/projects/:projectId", (req, res) => {
-  const { projectId } = req.params;
-  projectData
-    .getProjectById(projectId)
-    .then(data => res.send(data))
-    .catch(err => res.status(500).send(err));
-});
+    app.use((req, res, next) => {
+      res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+    });
 
-
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
-});
-
-app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}\n`+ `serving static files from:`, path.join(__dirname, 'public')));
+    app.listen(HTTP_PORT, () => {
+      console.log(`Server listening on port ${HTTP_PORT}`);
+      console.log(`Serving static files from:`, path.join(__dirname, "public"));
+    });
+  })
+  .catch(err => {
+    console.error("Failed to initialize project data:", err);
+    process.exit(1); 
+  });
