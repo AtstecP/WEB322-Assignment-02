df57b6a69263d6ee10bff903057451e326b33692
atstec <atstec80@gmail.com>
2025-03-05 19:18:59 -0500

    add ejs

--- server.js
+++ server.js
@@ -1,58 +1,73 @@
 /********************************************************************************
-* WEB322 – Assignment 02
-*
-* I declare that this assignment is my own work in accordance with Seneca's
-* Academic Integrity Policy:
-*
-* https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
-*
-* Name: Viktor Makarov Student ID: 121659221 Date: 1/31/2025*
-********************************************************************************/
+ * WEB322 – Assignment 02
+ *
+ * I declare that this assignment is my own work in accordance with Seneca's
+ * Academic Integrity Policy:
+ *
+ * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+ *
+ * Name: Viktor Makarov Student ID: 121659221 Date: 1/31/2025*
+ ********************************************************************************/
 require("dotenv").config();
 const projectData = require("./modules/projects");
+const developerData = require("./data/developerData");
+
 const express = require("express");
 const app = express();
 const path = require("path");
 const HTTP_PORT = process.env.PORT || 8080;
 
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 
-projectData.initialize()
+projectData
+  .initialize()
   .then(() => {
-    
     app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname, "/views/home.html"));
+      projectData
+        .getAllProjects()
+        .then((data) => res.render("home", { projects: data.slice(0, 3) }))
+        .catch((err) => res.status(500).send(err));
     });
 
     app.get("/about", (req, res) => {
-      res.sendFile(path.join(__dirname, "/views/about.html"));
+      res.render("about", { developer: developerData });
     });
 
+ 
     app.get("/solutions/projects", (req, res) => {
       const { sector } = req.query;
       if (sector) {
         projectData
           .getProjectsBySector(sector)
-          .then(data => res.send(data))
-          .catch(err => res.status(500).send(err));
+          .then((data) => res.render("projects", {projects: data}))
+          .catch((err) => res.status(404).render("404", {
+            error: `No projects found for sector: ${sector}`,
+          }));
       } else {
         projectData
           .getAllProjects()
-          .then(data => res.send(data))
-          .catch(err => res.status(500).send(err));
+          .then((data) => res.render("projects", {projects: data}))
+          .catch((err) => res.status(500).send(err));
       }
     });
+
 
     app.get("/solutions/projects/:projectId", (req, res) => {
       const { projectId } = req.params;
       projectData
         .getProjectById(projectId)
-        .then(data => res.send(data))
-        .catch(err => res.status(500).send(err));
+        .then((data) => res.render("project", {project: data}))
+        .catch((err) => res.status(404).render("404", {
+          error: "Unable to find requested project.",
+        }));
     });
 
     app.use((req, res, next) => {
-      res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+      res.status(404).render("404", {
+        error: "I'm sorry, we're unable to find what you're looking for.",
+      });
     });
 
     app.listen(HTTP_PORT, () => {
@@ -60,7 +75,7 @@ projectData.initialize()
       console.log(`Serving static files from:`, path.join(__dirname, "public"));
     });
   })
-  .catch(err => {
+  .catch((err) => {
     console.error("Failed to initialize project data:", err);
-    process.exit(1); 
+    process.exit(1);
   });
